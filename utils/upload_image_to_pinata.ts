22f4a41da5cc3b8c0f84dfc93fccb70803d28b30
yhy940806 <yhy940806@163.com>
2022-03-10 20:44:10 +0800

    wip: upload_image_to_pinata

--- utils/upload_image_to_pinata.ts
+++ utils/upload_image_to_pinata.ts
@@ -1,5 +1,15 @@
-const uploadImageToPinata = () => {
-  console.log("run uploadImageToPinata function...");
+const fs = require("fs");
+const pinataSDK = require("@pinata/sdk");
+require("dotenv").config({
+  path: "../.env",
+});
+const pinata = pinataSDK(
+  process.env.PINATA_API_KEY,
+  process.env.PINATA_API_SECRET
+);
+const pinataGatewayBaseUrl = "https://gateway.pinata.cloud/ipfs/";
+const uploadImageToPinata = (): string => {
+  return "";
 };
 
 module.exports = uploadImageToPinata;
