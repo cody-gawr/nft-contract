fadf41b5610c9511544efc5914f9c6494e759125
yhy940806 <yhy940806@163.com>
2022-03-10 17:58:44 +0800

    update a sample metadata

--- utils/metadata/sample_metadata.ts
+++ utils/metadata/sample_metadata.ts
@@ -1,11 +1,14 @@
 module.exports = {
-  name: "",
-  description: "",
-  image: "",
+  image:
+    "https://gateway.pinata.cloud/ipfs/QmSGMnEtg7oGQ9wzg9ASHoWNdrka9EFTFD6v4LZbDs1d4T",
+  name: "Waifu #1",
+  description:
+    "Holding this token will allow a user to successfully authenticate with Waifu, an up and coming NFT bot allowing users to automate the minting process with ease.\n\nEach token will have a different expiry date and time, the exact time this token expires can be found under the EXPIRY UNIX attribute.\n\nThis specific token expires on...\n\nFri, 24 Feb 2023 02:21:50 GMT",
   attributes: [
-    {
-      trait_type: "cutness",
-      value: 100,
-    },
+    { trait_type: "Expiry Date", value: "Fri, 24 Feb 2023 02:21:50 GMT" },
+    { trait_type: "Expiry UNIX", value: "1677205310" },
+    { trait_type: "Active", value: "true" },
   ],
+  external_url:
+    "https://etherscan.io/address/0x617b80414DfA15Da7F62238346022Aa643832028",
 };
